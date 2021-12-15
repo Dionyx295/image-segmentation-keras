805fc48e0876530be0f4cf7c5bd1bfe53fef45d1
Jean-Malo Chabin <jean.malo.chabin@live.fr>
2021-12-15 14:57:26 +0100

    no issue with prompt, closure fail if launched with spyder

---
+++ entrypoint.py
@@ -0,0 +1,12 @@
+"""Application entry point."""
+
+import sys
+from PyQt5.QtWidgets import QApplication
+
+from skyeye_segmentation.controller.main_window import MainWindow
+
+if __name__ == '__main__':
+    APP = QApplication(sys.argv)
+    W = MainWindow()
+    W.show()
+    sys.exit(APP.exec_())
